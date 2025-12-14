b9881ac8f90fbd6a0e1da8e9ec68f84bf1bf1f26
MuhammadSarfrazAfzal <sarfrazafzal790@gmail.com>
2025-12-14 10:46:47 +0500

    Setting work environment and folder strcture

---
+++ Source/client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <>
+      <p>this is navbar</p>
+    </>
+  )
+}
+
+export default NavBar
---
+++ Source/client/src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WelcomeScreen = () => {
+  return (
+    <>
+      <p>This is Welcome Screen</p>
+    </>
+  )
+}
+
+export default WelcomeScreen
